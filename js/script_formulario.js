0e93fabb623ca92812a40d42b18f38fddf58a947
samu3ak <samuelbolivar2003@gmail.com>
2022-10-04 12:18:46 +0200

    js tel validation + add email fields

--- js/script_formulario.js
+++ js/script_formulario.js
@@ -4,28 +4,60 @@ function $(selector) {
 }
 
 // No valid message function
-function noValidText(msg) {
+function noValidText(msg, clase) {
     let texto = document.createElement("p");
+    texto.className = clase;
     texto.innerHTML = msg;
     return texto;
 }
 
-// Adds another email field by clicking the email button
+// Insert after function
+function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+// Adds another email field by clicking the + button
 var showed = false;
-$("#buttonEmail").addEventListener("click", function () {
+$("#addEmailButton").addEventListener("click", function () {
     let numCampos = document.querySelectorAll("#campoCorreo");
     if (numCampos.length < 2) {
         var input_correo = document.createElement("input");
         input_correo.setAttribute("id", "campoCorreo");
         input_correo.setAttribute("type", "email");
         input_correo.setAttribute("name", "email");
-        input_correo.setAttribute("placeholder", "Tu correo");
-        $("#formulario").insertBefore(input_correo, $("#buttonEmail"));
+        input_correo.setAttribute("placeholder", "example@domain.com");
+        insertAfter(input_correo, $("input[type=email]"));
         showed = false;
     } else {
         if (!showed) {
-            $("#formulario").insertBefore(noValidText("No se permiten más de 3 correos"), $("#buttonEmail"));
+            insertAfter(noValidText("No se permiten más de 3 correos", "msgEmail"), $("#campoCorreo").nextSibling);
             showed = true;
         }
     }
 });
+
+// Remove one email field by clicking the - button, also removes the noValidText message, resetting it into unshowed
+$("#removeEmailButton").addEventListener("click", function () {
+    let elementos = document.querySelectorAll("#campoCorreo");
+    let i = elementos.length - 1;
+    elementos[i].remove();
+    $(".msgEmail").remove();
+    showed = false;
+});
+// Validates de telephone number
+insertAfter(noValidText("Número de teléfono incorrecto", "msgTel"), $("input[name=telefono]"));
+$(".msgTel").style.display = "none";
+var telRegex = /^(\d{9})$/;
+function telCheck() {
+    if (!$("input[name=telefono]").value.replaceAll(" ", "").match(telRegex)) {
+        $(".msgTel").style.display = "block";
+    } else {
+        $(".msgTel").style.display = "none";
+    }
+}
+
+// Validates email
+
+
+// Checks if it's possible to submit the form
+$("button[type=submit]").disabled = true;
